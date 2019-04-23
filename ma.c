58dc624f81c9f8c95d72a8fac25392165fb9803e
Helena Martins <lenamartiins@gmail.com>
2019-04-23 17:58:46 +0100

    ficheiros

---
+++ ma.c
@@ -0,0 +1,21 @@
+//inserir novos artigos ou modificar atributos de artigos.
+
+#include "ma.h"
+
+
+
+//alterar nome do artigo
+// n <código> <novo nome>
+
+
+//alterar preço do artigo
+// p <código> <novo preço>
+
+
+
+//inserir artigos
+// i <nome> <preço>
+
+void insereArtigo(){
+  int fd = open(ARTIGOS, O_CREAT|O_APPEND|O_RDWR, 0777);
+}
