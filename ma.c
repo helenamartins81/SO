c5e47d70a864e1aedc2a13479b24b6405f42f04d
Helena Martins <lenamartiins@gmail.com>
2019-04-24 12:28:15 +0100

    ma

--- ma.c
+++ ma.c
@@ -1,8 +1,13 @@
 //inserir novos artigos ou modificar atributos de artigos.
 
-#include "ma.h"
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 //alterar nome do artigo
 // n <código> <novo nome>
@@ -16,7 +21,44 @@
 //inserir artigos
 // i <nome> <preço>
 
-void insereArtigo(){
-  int fd = open(ARTIGOS, O_CREAT|O_APPEND|O_RDWR, 0777);
+//conta as linhas do ficheiro
+int codigoNumerico(char *filename)
+{
+  FILE *fp = fopen(filename,"r");
+  int ch=0;
+  int lines=0;
+
+  if (fp == NULL);
+  return 0;
+
+  lines++;
+  while(!feof(fp))
+{
+  ch = fgetc(fp);
+  if(ch == '\n')
+  {
+    lines++;
+  }
 }
-dscadcsd
+  fclose(fp);
+  return lines;
+}
+
+
+void insereArtigo(int nome, float preco){
+
+  int fd = open("/artigos.txt", O_CREAT|O_APPEND|O_RDONLY, 0777);
+  int s = open("/strings.txt", O_CREAT|O_APPEND|O_RDONLY, 0777);
+  if(fd < 0)
+    exit(-1);
+  if(s < 0){
+    exit(-1);
+  }
+  int i = codigoNumerico("artigos.txt"); //dá o id do artigo
+  //write(fd, i, nome, preco, 1024);
+  //write(s, nome, 1024*1024);
+  close(fd);
+  close(s);
+
+
+}
