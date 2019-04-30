cd3cea516d2d95986c98f4bb44587f1c55114b02
Helena Martins <lenamartiins@gmail.com>
2019-04-30 18:01:23 +0100

    contalinhas

--- ma.c
+++ ma.c
@@ -10,47 +10,36 @@
 #include <sys/wait.h>
 
 
-#define BUFFER_SIZE 1024
+#define SIZE 30
+#define SIZE_STRING 50
 
 //alterar nome do artigo
 // n <código> <novo nome>
 
-
-void altera_string(int c, char nome[10]);
-
-void altera_nome(int code, char nome[10]){
-
-  int fd = open("/artigos.txt", O_RDWR, 0600);
-  if(fd < 0)
-    exit(-1);
-  int i = 0;
-  char linha[BUFFER_SIZE];
-  while(read(fd, linha, sizeof linha) >0){
-    if(i == code){
-      int c = linha[2];
-      altera_string(c, nome);
-    }
-    else i++;
-  }
-  close(fd);
-
+void preenche_espacos (char buf[SIZE_STRING]){
+      int i = strlen(buf);
+      for(;i<SIZE_STRING-1;i++){
+        buf[i]='a';
+      }
+      buf[i]='\n';
 }
 
 
-void altera_string(int c, char nome[10]){
-  int fd = open("/artigos.txt", O_RDWR, 0600);
-  if(fd < 0)
+//mudar para tamanho do nome variavel
+int altera_nome(int code, char* nome){
+  int fd = open("strings.txt", O_CREAT|O_WRONLY, 0777);
+  if(fd<0){
     exit(-1);
-  int i = 0;
-  char linha[BUFFER_SIZE];
-  while(read(fd, linha, sizeof linha) >0){
-    if(i == c){
-      lseek(fd, -sizeof(linha[2]),SEEK_END);
-      write(fd, nome, strlen(nome));
-    }
-    else i++;
   }
+  lseek(fd,SIZE_STRING*code, SEEK_SET);
+  char buf[SIZE_STRING];
+  sprintf(buf, "nome: %s", nome);
+  preenche_espacos(buf);
+  write(fd, buf,strlen(buf));
+
   close(fd);
+return 0;
+
 }
 
 
@@ -58,53 +47,49 @@ void altera_string(int c, char nome[10]){
 
 //alterar preço do artigo
 // p <código> <novo preço>
-//NAO FAÇO IDEIA SE ISTO FAZ ALGUMA COISA DECENTE
-void alteraPreco(int code, float preco){
-    char buf[10];
-    gcvt(preco, 6, buf);
-    int fd = open("/artigos.txt", O_RDWR, 0600);
-    if(fd < 0)
+//NAO ESTA A DAR ???
+
+int altera_preco(int code, float preco){
+    int fd = open("artigos.txt", O_RDWR, 0777);
+    if(fd < 0){
       exit(-1);
-    int i = 0;
-    char linha[BUFFER_SIZE];
-    while(read(fd, linha, sizeof linha) >0){
-      if(i == code){
-        lseek(fd, -sizeof(float),SEEK_END);
-        write(fd, buf, sizeof(float));
-      }
-      else i++;
     }
+    lseek(fd, SIZE*code , SEEK_SET);
+    char buffer[SIZE];
+    sprintf(buffer, "artigo: %d, preco: %f\n", code, preco);
+    write(fd, buffer, strlen(buffer));
+
     close(fd);
+    return 0;
 }
 
+
+
+
+
+int countLines(){
+  int fd = open("artigos.txt", O_RDONLY, 0777);
+  if(fd<0){
+    exit(-1);
+  }
+  int count= 0;
+  char c;
+  while(read(fd, &c, 1)==1){
+    if(c=='\n'){
+      count++;
+    }
+  }
+
+  close(fd);
+  return count;
+}
 
 
 //inserir artigos
 // i <nome> <preço>
 
-//conta as linhas do ficheiro
-int codigoNumerico(char *filename){
-  FILE *fp = fopen(filename,"r");
-  int ch=0;
-  int lines=0;
-
-  if (fp == NULL);
-  return 0;
-
-  lines++;
-  while(!feof(fp)){
-    ch = fgetc(fp);
-    if(ch == '\n'){
-      lines++;
-    }
-  }
-  fclose(fp);
-  return lines;
-}
-
-
 void insereArtigo(int nome, float preco){
-  char buf[BUFFER_SIZE];
+  char buf[SIZE];
   int fd = open("/artigos.txt", O_CREAT|O_APPEND|O_RDONLY, 0777);
   int s = open("/strings.txt", O_CREAT|O_APPEND|O_RDONLY, 0777);
   if(fd < 0)
@@ -112,12 +97,17 @@ void insereArtigo(int nome, float preco){
   if(s < 0){
     exit(-1);
   }
-  int i = codigoNumerico("artigos.txt"); //dá o id do artigo
-  sprintf(buf, "%d %d %f", i, nome, preco);
-  write(fd, buf, 40);
-  //write(s, nome, 1024*1024); -> escrever no ficheiro strings
+  int count = countLines();
+  
+
   close(fd);
   close(s);
 
+}
+
+
+/*
+int main(){
 
 }
+*/
